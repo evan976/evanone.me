0ec6a75b1cc86c3a7e1875872292b3143589c7f2
evan <jihuawu118@gmail.com>
2024-04-11 22:56:35 +0800

    Completed home page

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -62,7 +62,7 @@ function CodeIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-6 md:space-y-0 dark:text-neutral-300">
+      <ul className="mt-8 flex text-neutral-600 space-x-6 dark:text-neutral-300">
         <li>
           <a
             className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -14,13 +14,13 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className='className="-ml-2 mb-16 tracking-tight"'>
+    <aside className="-ml-2 mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex space-x-0 pr-10">
+          <div className="flex space-x-2 md:space-x-4 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
